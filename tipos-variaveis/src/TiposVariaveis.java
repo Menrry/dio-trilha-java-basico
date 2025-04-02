0977e220e9dce38cda852724b24102585ac41422
Menrry <msantanalacruz@gmail.com>
2025-04-02 20:25:32 -0300

    Operador soma e concatenacao

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     
+     
+     // Adaptación add cast to short
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        Short numeroCurto2 = (short) numeroNormal;
+
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao );
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao );
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao );
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao );
+
+        concatenacao = "1"+ (1+1+1);
+        System.out.println(concatenacao );
+
+                 
     }
 }
