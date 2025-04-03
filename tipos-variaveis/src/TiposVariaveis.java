02695861944558fb31d2e71b7a9c9b4d07983dda
Menrry <msantanalacruz@gmail.com>
2025-04-03 12:19:37 -0300

    Agregando operadores y uso de metodos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,7 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
      
-     
+ /**** 
      // Adaptación add cast to short
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
@@ -24,6 +24,39 @@ public class TiposVariaveis {
         concatenacao = "1"+ (1+1+1);
         System.out.println(concatenacao );
 
-                 
-    }
+*/    
+/** 
+// Operadores Unarios
+        int numero = 5;
+
+        numero = - numero;
+        System.out.println(numero);
+
+        numero = - numero;  // tambien numero = numero * -1
+        System.out.println(numero);    
+*/      
+
+
+/* 
+// Operadores Ternários
+        int a,b;
+        a=5;
+        b=5;
+
+        String resultado = a==b ?"verdadeiro" : "falso";
+
+        System.out.println(resultado);
+   
+ */
+
+ // Operadores Relacionais
+    int numero1 = 2;
+    int numero2 = 2;
+    
+    boolean simNao = numero1 ==numero2;
+    
+    System.out.println("Numero é igual a numeroDois? " + simNao );
+   
+   
+     }
 }
